82de590aa62b6deccc3e0a5a74ac3d43947014d4
alcoholisment <alcoholism1025@gmail.com>
2020-10-01 13:01:33 +0900

    調整

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,4 @@ class Message < ApplicationRecord
   validates :content, presence: true, length: { maximum: 200 }
   after_create_commit { MessageBroadcastJob.perform_later self }
   belongs_to :room
-en
+end
